65830e0fe6bc89756de9f16cf144987678dd08a7
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-13 18:56:56 +0200

    Update

--- src/components/structures/footer/footer.component.jsx
+++ src/components/structures/footer/footer.component.jsx
@@ -3,7 +3,35 @@ import './footer.style.scss';
 
 export const Footer = () => (
   <footer>
-    Made with ❤️ by AfricLite <time>{new Date().getFullYear()}</time>
+    <span className="social-media-wrapper">
+      <a
+        href="https://www.instagram.com/creativedevelopals/"
+        target="_blank"
+        rel="noreferrer"
+        aria-describedby="instagramLink"
+      >
+        <span id="instagramLink" style={{display: 'none'}} aria-hidden={true}>
+          Link to Discovery1 founders instagram page
+        </span>
+        <i class="fab fa-instagram-square"></i>
+      </a>
+
+      <a
+        href="https://www.facebook.com/creativedevelopals"
+        target="_blank"
+        rel="noreferrer"
+        aria-describedby="facebookLink"
+      >
+        <span id="facebookLink" style={{display: 'none'}} aria-hidden={true}>
+          Link to Discovery1 founders facebook page
+        </span>
+        <i class="fab fa-facebook-square"></i>
+      </a>
+    </span>
+
+    <span>
+      Made with ❤️ by CreativeDevelopals <time>{new Date().getFullYear()}</time>
+    </span>
   </footer>
 );
 
